f9020f0fa9db6490d28c9d7142dd2795af11759b
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-16 15:22:56 +0530

    VolleyDemo project got one more activity which is stringobject from volley

--- VolleyDemo/app/src/main/java/com/example/volleydemo/ChoiceActivity.java
+++ VolleyDemo/app/src/main/java/com/example/volleydemo/ChoiceActivity.java
@@ -2,13 +2,33 @@ package com.example.volleydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChoiceActivity extends AppCompatActivity {
 
+    private Button arrayObject,stringObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        arrayObject=findViewById(R.id.arrayObject);
+        arrayObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ChoiceActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+        stringObject=findViewById(R.id.stringObject);
+        stringObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ChoiceActivity.this,StringObjectActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ VolleyDemo/app/src/main/java/com/example/volleydemo/StringObjectActivity.java
@@ -0,0 +1,41 @@
+package com.example.volleydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Text;
+
+public class StringObjectActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String url="https://jsonplaceholder.typicode.com/todos/1";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_string_object);
+        textView=findViewById(R.id.textview);
+        RequestQueue requestQueue;
+        requestQueue= Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("Got some problem while fetching the response.");
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+}
